368cc8225d1aa1b641d0d0d523a14faae926d4b9
adariori <adariori3@gmail.com>
2026-08-12 09:42:36 +0100

    Ajoute la signature d'authorship dans src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,19 @@
+/**
+ * @author    ARIORI OLOROUNKO Adéliyi Odjouola Moshood
+ * @github    https://github.com/adariori
+ * @web       https://portefolio-nine-iota.vercel.app/
+ * @contact   adariori3@gmail.com
+ * @location  Cotonou, Benin
+ *
+ * @project   Dev Portfolio
+ * @version   0.0.0
+ * @year      2026
+ *
+ * @license   MIT License
+ *            © 2026 ARIORI OLOROUNKO Adéliyi Odjouola Moshood
+ *            Voir le fichier LICENSE à la racine du projet.
+ */
+
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
